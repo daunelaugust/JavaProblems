53be7ca7d4b01f808f71d9a2e9735dd57cbe9230
daunelaugust <daunelaugustin@gmail.com>
2023-04-14 06:22:10 -0400

    twosum

---
+++ JavaProblems/src/TwoSum.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class TwoSum {
+        public int[] twoSum(int[] nums, int target) {
+    	int[] sum = new int[2];
+        Map<Integer, Integer> m = new HashMap();
+        for (int i = 0; i<nums.length; i++){
+            if (m.get(nums[i])!= null){
+               sum = new int [] {m.get(nums[i]), i};
+            }else{
+                m.put(target - nums[i],i);
+            }
+}
+return sum;
+}
+}
